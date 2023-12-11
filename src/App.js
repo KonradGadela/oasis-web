9729ab4cd473c550454eb46b126962b391cbca54
Karol <ka.zajaczkowski@student.po.edu.pl>
2023-12-11 23:14:06 +0100

    Added RoomPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+import Rooms from './components/Rooms/Rooms';
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/rooms" element={<Rooms />} />
+
       </Routes>
     </Router>
   );
