c788662c13369ccf45d09ce95af8e9a1993a6a57
Konrad Gądela <konradgadela@gmail.com>
2023-12-14 20:36:42 +0100

    rest

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './NavBar';
 import { Link } from 'react-router-dom';
 
 
 
 const SignIn = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
     <div>
       <NavBar />
@@ -14,18 +19,37 @@ const SignIn = () => {
         <h2>Welcome back!</h2>
 
         <div class="sign-in-inputs">
+
           <div class="mb-3">
             <label for="formGroupExampleInput" class="form-label sign-up-label">Address email:</label>
-            <input type="text" class="sign-up-label form-control" id="formGroupExampleInput" placeholder="" />
+            <input
+              type="text"
+              class="sign-up-label form-control"
+              id="formGroupExampleInput"
+              placeholder="" 
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              />
           </div>
 
           <div class="mb-3">
             <label for="inputPassword4" class="form-label">Password:</label>
-            <input type="password" class="sign-up-label form-control" id="inputPassword4" />
+            <input
+              type="password"
+              class="sign-up-label form-control"
+              id="inputPassword4"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
+
         </div>
 
-        <button type="button" class="btn btn-success log-in-button"><Link to="/SignUp" className="custom-link" >Sign In</Link></button>
+        <button
+          type="button"
+          class="btn btn-success log-in-button">
+          <Link to="/SignUp" className="custom-link" >Sign In</Link>
+        </button>
 
       </div>
 
